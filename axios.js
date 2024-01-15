6d089fbb3e693c1fb20afea01d77021b1a0f49e6
Nitin srivastava <nitinsrivaspaa445@gmail.com>
2024-01-15 16:52:44 +0530

    New File Pushed

---
+++ axios.js
@@ -0,0 +1,19 @@
+let btn=document.querySelector("button");
+btn.addEventListener("click", async ()=>{
+let fact=await getFact();
+// console.log(fact);
+let p=document.querySelector("#result");
+p.innerText=fact;
+});
+let url="https://catfact.ninja/fact";
+async function getFact() {
+    try{
+        let res=await axios.get(url);
+        // console.log(res.data.fact);
+        return res.data.fact;
+    }
+    catch(e){
+        console.log("error-",e);
+        return "No fact found";
+    }
+}
